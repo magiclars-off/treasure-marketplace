e3d9cb7e3882e3ce2627fe73412caa254cb7f786
Neil Kistner <neil.kistner@gmail.com>
2022-04-04 09:42:16 -0500

    Fix not being able to click info icon

--- src/pages/collection/[address]/index.tsx
+++ src/pages/collection/[address]/index.tsx
@@ -1402,15 +1402,7 @@ const Collection = () => {
                             normalizedLegion ?? realmStats ?? elleriaStats;
 
                           return (
-                            <li key={listing.id} className="group relative">
-                              <Link
-                                href={`/collection/${slugOrAddress}/${listing.token.tokenId}`}
-                                passHref
-                              >
-                                <a className="absolute inset-0 w-full h-full">
-                                  <span className="sr-only">View Details</span>
-                                </a>
-                              </Link>
+                            <li key={listing.id} className="group">
                               <div className="block w-full aspect-w-1 aspect-h-1 rounded-sm overflow-hidden focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-red-500">
                                 {metadata ? (
                                   <>
