3579335a73023b9067261d31e82cd459be38e582
Yuya Oiwa <yuya.oiwa@me.com>
2022-04-06 10:07:00 +0900

    reorder index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,69 @@ import Link from "next/link";
 
 const collections = [
   {
+    href: "legion-genesis",
+    name: "Legion Genesis",
+    image:
+      "https://ipfs.io/ipfs/QmRqosGZZ6icx6uSDjLuFFMJiWDefZAiAZdpJdBK9BP5S4/Warlock.png",
+    description: "The Origin Legions of Bridgeworld with a fixed supply.",
+  },
+  {
+    href: "smol-brains",
+    name: "Smol Brains",
+    image:
+      "https://ipfs.io/ipfs/QmY71ban6QoWg9nbNwikk6wVWknj8NFBG8nMGHEuzwfAwf/121/0.png",
+    description:
+      "The Smol Brains are a dynamic PFP of a monkey whose head gets bigger the larger its IQ becomes.",
+  },
+  {
+    href: "seed-of-life",
+    name: "Seed of Life",
+    image:
+      "https://ipfs.io/ipfs/QmbkpUo9dPsTVDfttdgkV6eqbPLCXyoKhFBxhwdAgqB15z/Seed of Life 1.png",
+    description:
+      "Built atop the Magic ecosystem, Life embodies the metaverse as a living breathing ecosystem...",
+  },
+  {
+    href: "battlefly",
+    name: "BattleFly",
+    image:
+      "https://ipfs.io/ipfs/QmVSMdABd2hahtS74owdeu6nHauaXhWviReyMGch8Ztb6W",
+    description:
+      "BattleFly is an experimental PVP/P2E strategy game, powered by $MAGIC.",
+  },
+  {
+    href: "legion-auxiliary",
+    name: "Legion Auxiliary",
+    image:
+      "https://ipfs.io/ipfs/QmTxcMUqVvrHMrdLUqtSjFSbHZ4ZoQ2bUB6U7CEpA7JDiF/Uncommon Fighter.gif",
+    description:
+      "Descendants of Genesis Legions that can be summoned in Bridgeworld.",
+  },
+  {
+    href: "treasures",
+    name: "Treasures",
+    image:
+      "https://ipfs.io/ipfs/Qmbyy8EWMzrSTSGG1bDNsYZfvnkcjAFNM5TXJqvsbuY8Dz/Honeycomb.gif",
+    description:
+      "Treasures are composable building blocks in Bridgeworld that will be used inter- and intra-metaverse.",
+  },
+  {
+    href: "consumables",
+    name: "Consumables",
+    image:
+      "https://ipfs.io/ipfs/QmdpMJMTRrGu1Z43RF94WnDRj5QwiLJXo43TwKWPX2cTWE/Medium Prism.gif",
+    description:
+      "Functional items that are crafted from Treasures and give utility in the Metaverse.",
+  },
+  {
+    href: "smol-bodies",
+    name: "Smol Bodies",
+    image:
+      "https://ipfs.io/ipfs/QmSqwxNFMeFtgdCnjBTTixx46Wi6TH9FtQ5jAp98JnAoeR/3948/0.png",
+    description:
+      "The Smol Bodies inhabit a gym near you, stacking $plates to earn muscle and be not smol.",
+  },
+  {
     href: "smithonia-weapons",
     name: "Smithonia Weapons",
     image:
@@ -21,28 +84,6 @@ const collections = [
       "Tales of Elleria is an immersive three-dimensional role-playing GameFi project. Summon heroes, take on assignments, go on quests and epic adventures to battle dangerous monsters earn tremendous rewards.",
   },
   {
-    href: "realm",
-    name: "Realm",
-    image: "/img/realm.png",
-    description:
-      "Realm is a decentralized world-building experience. Enjoy $MAGIC emissions and Loot from across the Metaverse.",
-  },
-  {
-    href: "battlefly",
-    name: "BattleFly",
-    image:
-      "https://ipfs.io/ipfs/QmVSMdABd2hahtS74owdeu6nHauaXhWviReyMGch8Ztb6W",
-    description:
-      "BattleFly is an experimental PVP/P2E strategy game, powered by $MAGIC.",
-  },
-  {
-    href: "legion-genesis",
-    name: "Legion Genesis",
-    image:
-      "https://ipfs.io/ipfs/QmRqosGZZ6icx6uSDjLuFFMJiWDefZAiAZdpJdBK9BP5S4/Warlock.png",
-    description: "The Origin Legions of Bridgeworld with a fixed supply.",
-  },
-  {
     href: "peek-a-boo",
     name: "Peek-A-Boo",
     image: "/img/peekaboo.svg",
@@ -50,28 +91,11 @@ const collections = [
       "Peek-A-Boo is a collection of 10,000 adorable NFTs. There are two types, Ghosts and Busters.",
   },
   {
-    href: "smol-bodies",
-    name: "Smol Bodies",
-    image:
-      "https://ipfs.io/ipfs/QmSqwxNFMeFtgdCnjBTTixx46Wi6TH9FtQ5jAp98JnAoeR/3948/0.png",
+    href: "realm",
+    name: "Realm",
+    image: "/img/realm.png",
     description:
-      "The Smol Bodies inhabit a gym near you, stacking $plates to earn muscle and be not smol.",
-  },
-  {
-    href: "legion-auxiliary",
-    name: "Legion Auxiliary",
-    image:
-      "https://ipfs.io/ipfs/QmTxcMUqVvrHMrdLUqtSjFSbHZ4ZoQ2bUB6U7CEpA7JDiF/Uncommon Fighter.gif",
-    description:
-      "Descendants of Genesis Legions that can be summoned in Bridgeworld.",
-  },
-  {
-    href: "smol-brains",
-    name: "Smol Brains",
-    image:
-      "https://ipfs.io/ipfs/QmY71ban6QoWg9nbNwikk6wVWknj8NFBG8nMGHEuzwfAwf/121/0.png",
-    description:
-      "The Smol Brains are a dynamic PFP of a monkey whose head gets bigger the larger its IQ becomes.",
+      "Realm is a decentralized world-building experience. Enjoy $MAGIC emissions and Loot from across the Metaverse.",
   },
   {
     href: "smol-brains-pets",
@@ -82,14 +106,6 @@ const collections = [
       "The Smol Brains Pets are cute companions to accompany your Smol Brain in Smolverse.",
   },
   {
-    href: "seed-of-life",
-    name: "Seed of Life",
-    image:
-      "https://ipfs.io/ipfs/QmbkpUo9dPsTVDfttdgkV6eqbPLCXyoKhFBxhwdAgqB15z/Seed of Life 1.png",
-    description:
-      "Built atop the Magic ecosystem, Life embodies the metaverse as a living breathing ecosystem...",
-  },
-  {
     href: "smol-cars",
     name: "Smol Cars",
     image:
@@ -97,14 +113,7 @@ const collections = [
     description:
       "The Smol Cars are here to get you around in Smolverse. Vroom vroom.",
   },
-  {
-    href: "treasures",
-    name: "Treasures",
-    image:
-      "https://ipfs.io/ipfs/Qmbyy8EWMzrSTSGG1bDNsYZfvnkcjAFNM5TXJqvsbuY8Dz/Honeycomb.gif",
-    description:
-      "Treasures are composable building blocks in Bridgeworld that will be used inter- and intra-metaverse.",
-  },
+
   {
     href: "smol-treasures",
     name: "Smol Treasures",
@@ -112,14 +121,6 @@ const collections = [
       "https://ipfs.io/ipfs/QmZK1i4y7qn7Fi7mEMgT4KZcb1Etb12yndcTZ5dnhigDPt/2.gif",
     description:
       "Smols and Swols are currently farming Smol treasures on the moon.",
-  },
-  {
-    href: "consumables",
-    name: "Consumables",
-    image:
-      "https://ipfs.io/ipfs/QmdpMJMTRrGu1Z43RF94WnDRj5QwiLJXo43TwKWPX2cTWE/Medium Prism.gif",
-    description:
-      "Functional items that are crafted from Treasures and give utility in the Metaverse.",
   },
 ];
 
