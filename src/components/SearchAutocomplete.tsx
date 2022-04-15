f07a28074c330f0407cb761c94f6614b0a9a1a6c
caesarTheApe <103601277+caesarTheApe@users.noreply.github.com>
2022-04-15 11:07:10 -0300

    Focus search input when search icon clicked (#236)

--- src/components/SearchAutocomplete.tsx
+++ src/components/SearchAutocomplete.tsx
@@ -212,7 +212,13 @@ export function SearchAutocomplete<T extends object>(props: Props<T>) {
             : "border-gray-300 dark:border-gray-500"
         }`}
       >
-        <SearchIcon aria-hidden="true" className="w-5 h-5 text-gray-500" />
+        <button {...buttonProps} ref={clearButtonRef}>
+          <SearchIcon
+            aria-hidden="true"
+            className={`w-5 h-5
+            ${state.isFocused ? "text-gray-300" : "text-gray-500"}`}
+          />
+        </button>
         <input
           {...inputProps}
           ref={inputRef}
